bea8524cddaac9c2a2b1081a89ba2958a665aa52
Ridha <elb.ridha@gmail.com>
2015-04-10 16:50:00 +0200

    Corrections, add option screen, add sound ids, correction taille police texte scénarios

---
+++ Assets/MoveButtonHandler.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MoveButtonHandler : MonoBehaviour
+{
+    public Sprite deplacement;
+    public Sprite fuite;
+
+    GameManager gm;
+    bool isAllowedToMove;
+
+    void Start()
+    {
+        isAllowedToMove = true;
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    public void UpdateState()
+    {
+        if (gm.EM.Scenario.GetCurrentPlayer().CurrentSquare.IsThreatened(gm.EM.Scenario.GetCurrentPlayer()))
+        {
+            GetComponent<Image>().sprite = fuite;
+            isAllowedToMove = false;
+        }
+        else
+        {
+            isAllowedToMove = true;
+            GetComponent<Image>().sprite = deplacement;
+        }
+    }
+
+    public void EnableButtonAction()
+    {
+        if (isAllowedToMove)
+        {
+            gm.Move();
+        }
+        // TODO call flee function of GM
+        //else 
+        //{
+        //    gm.Flee();
+        //}
+    }
+
+    
+}
