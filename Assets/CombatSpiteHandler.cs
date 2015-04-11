154f4d700da832f0623ed5195351fc528e8ab7ed
Ridha El Bekri <elb.ridha@gmail.com>
2015-04-11 14:04:39 +0200

    Integration PART 1 de combat screen

---
+++ Assets/CombatSpiteHandler.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatSpiteHandler : MonoBehaviour
+{
+
+    public GameObject niles;
+    public GameObject volotom;
+    public GameObject cat;
+    public GameObject souris;
+    public GameObject filou;
+
+    public void UpdateSate(string id)
+    {
+        Clear();
+        switch (id)
+        {
+            case "volotom":
+                volotom.SetActive(true);
+                break;
+            case "niles":
+                niles.SetActive(true);
+                break;
+            case "souris":
+                souris.SetActive(true);
+                break;
+            case "filou":
+                filou.SetActive(true);
+                break;
+            case "cat":
+                cat.SetActive(true);
+                break;
+            default:
+                Debug.LogError("Unknown ID for Target Sprite Manager");
+                break;
+        }
+    }
+
+    void Clear()
+    {
+        niles.SetActive(false);
+        volotom.SetActive(false);
+        cat.SetActive(false);
+        souris.SetActive(false);
+        filou.SetActive(false);
+    }
+
+
+}
